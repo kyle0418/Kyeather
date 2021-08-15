ed21313fa1313d997396126a4c5ae169996b9e2c
Kyle <1396676171@qq.com>
2021-08-15 12:27:25 +0800

    Add files via upload

---
+++ Source/Kyeather/Kyeather.Android/SplashActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Kyeather.Droid
+{
+    [Activity(Label = "Kyeather", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait,
+    LaunchMode = LaunchMode.SingleTop)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+
+            //if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            //{
+            //    Window.DecorView.SystemUiVisibility = StatusBarVisibility.Visible;
+            //    Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            //}
+
+            //InvokeMainActivity();
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(200); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        //private void InvokeMainActivity()
+        //{
+        //    var mainActivityIntent = new Intent(this, typeof(MainActivity));
+        //    mainActivityIntent.AddFlags(ActivityFlags.NoAnimation); //Add this line
+        //    StartActivity(mainActivityIntent);
+        //}
+    }
+}
