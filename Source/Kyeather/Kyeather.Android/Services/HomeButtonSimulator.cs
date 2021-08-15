2b5936d478b932d96cb4bdd9d6d761fd55cdfd2e
Kyle <1396676171@qq.com>
2021-08-15 12:29:04 +0800

    Add files via upload

---
+++ Source/Kyeather/Kyeather.Android/Services/HomeButtonSimulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Kyeather.Droid.Services;
+using Kyeather.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HomeButtonSimulator))]
+namespace Kyeather.Droid.Services
+{
+    class HomeButtonSimulator : IHomeButtonSimulator
+    {
+        public void SimulateHomeButton()
+        {
+            var activity = (Activity)Forms.Context;
+            activity.MoveTaskToBack(true);
+        }
+    }
+}
