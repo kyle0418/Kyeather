9e2c81d9180b48ae37ac32934e1386c422ce93ac
Kyle <1396676171@qq.com>
2021-08-15 12:23:02 +0800

    Add files via upload

---
+++ Source/Kyeather/Kyeather/Services/IHomeButtonSimulator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kyeather.Services
+{
+    public interface IHomeButtonSimulator
+    {
+        void SimulateHomeButton();
+    }
+}
